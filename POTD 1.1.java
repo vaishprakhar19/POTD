29bfc359d98e5cd8544903b197ab3cb2e2aac8e2
vaishprakhar19 <126570561+vaishprakhar19@users.noreply.github.com>
2023-09-24 22:07:22 +0530

    Update POTD 1.1.java

--- POTD 1.1.java
+++ POTD 1.1.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 public class Main {
-
-  public static void main(String[] args)
-
-			{
-   	          int i,j,n;
-
-               System.out.print("Input number of n : ");
+  public static void main(String[] args){
+   	        int i,j,n;
+                System.out.print("Input number of n : ");
             	Scanner in = new Scanner(System.in);
-		    n = in.nextInt();
-  
-               for(i=1;i<=n;i++)
-               {
-	           for(j=1;j<=i;j++)
-	            System.out.print(i);
-	          System.out.println("");
-              }
+		n = in.nextInt();
+                
+	  for(i=1;i<=n;i++){
+	           
+		  for(j=1;j<=i;j++)
+	           System.out.print(i);
+	           System.out.println("");
+                 }
             }
-            } 
+        } 
