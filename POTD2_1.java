4d8ceb637bfabb1d8f8c95c2b10407071ff52fc6
vaishprakhar19 <126570561+vaishprakhar19@users.noreply.github.com>
2023-10-02 11:48:28 +0530

    Update POTD2_1.java

--- POTD2_1.java
+++ POTD2_1.java
@@ -4,12 +4,22 @@ public class POTD2_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 64 + n; j >=65+n-i; j--) {
-                System.out.printf("%c", j);
+        
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=i;j--){
+                // System.out.printf("%c", j+64);
+                System.out.print((char)(j+64));
             }
-           System.out.println();
+            System.out.println();
         }
+        
         sc.close();
     }
 }
+
+// for (int i = 1; i <= n; i++) {
+//     for (int j = 64 + n; j >=65+n-i; j--) {
+//         System.out.printf("%c", j);
+//     }
+//     System.out.println();
+// }
